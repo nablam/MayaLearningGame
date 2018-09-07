250a4252a0be496f7367af89d023df883292349c
nablam <lamribenn@gmail.com>
2018-09-07 01:20:20 -0400

    working dino ctrl

---
+++ Assets/_Scripts/DinoCTRL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoCTRL : MonoBehaviour {
+
+    Animator _animator;
+        void Start () {
+        _animator = this.transform.GetChild(0).gameObject.GetComponent<Animator>();
+	}
+    MyEnums.DinoState _curDinoStateState;
+    void UpdateANimatorState() {
+        _animator.SetInteger("DinoState", (int)_curDinoStateState);
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) { _curDinoStateState = MyEnums.DinoState.Idle; }
+        else
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { _curDinoStateState = MyEnums.DinoState.Walking; }
+        else
+        if (Input.GetKeyDown(KeyCode.Space)) { _animator.SetTrigger("TrigEat"); }
+        UpdateANimatorState();
+
+    }
+}
---
+++ Assets/_Scripts/MyEnums.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MyEnums : MonoBehaviour {
+
+    public enum ColliderTypes
+    {
+        Type_Maya,
+        Type_Julie,
+        Type_Nabil,
+        Type_Elmo,
+        Type_Cmonster,
+        Type_Lion,
+        Type_Pasha,
+        Type_Minnie,
+		Type_Mickey,
+		Type_Mouminek,
+		Type_Foxy,
+		Type_LittleBrother,
+		Type_Froggy,
+		Type_Zoe
+    }
+
+    public enum DinoState
+    {
+        Idle,
+        Walking
+    }
+}
