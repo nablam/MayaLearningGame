07141eb3f3ce240612fb862434a31561657d55fe
nablam <lamribenn@gmail.com>
2018-09-08 01:27:46 -0400

    dino can 1waitforfood, 2facefood ,3movetoit ,4eatit . as long as food is within detection . now need to omplement walk backwards fir the corner case stated above

--- Assets/_Scripts/DinoBrain.cs
+++ Assets/_Scripts/DinoBrain.cs
@@ -6,8 +6,10 @@ public class DinoBrain : MonoBehaviour {
     public GameObject FoodTargetObject;
     public bool _moveToFoodUpdate = false;
     int FoodDirection = 0;
+    float DistToFood = -1f; //enforce negative value as NOFOOD
     float speed = 0.038f;
     Animator _animator;
+    MyEnums.DinoState _curState;
     void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
@@ -17,8 +19,9 @@ public class DinoBrain : MonoBehaviour {
     //WaitForFood
     public void Action_WaitForFood()
     {
+        _curState = MyEnums.DinoState.Idle;
         _animator.speed = 1;
-        _animator.SetInteger("DinoState", (int)MyEnums.DinoState.Idle);
+        _animator.SetInteger("DinoState", (int)_curState);
         _moveToFoodUpdate = false;
     }
     //HereIsSomeAmAM
@@ -35,11 +38,37 @@ public class DinoBrain : MonoBehaviour {
     //MoveToFood
     public void Action_MoveToFood()
     {
+        _curState = MyEnums.DinoState.Walking;
         _moveToFoodUpdate = true;
-        _animator.SetInteger("DinoState", (int)MyEnums.DinoState.Walking);
         _animator.speed = 1;
+        _animator.SetInteger("DinoState", (int)_curState);
     }
     //EatFood
+    public void Action_EatFood() {
+        Debug.Log("nom nom nom");
+        _moveToFoodUpdate = false;
+        _animator.speed = 1;
+        _animator.SetTrigger("TrigEat");
+    }
+    void CheckDistIfPossible()
+    {
+        if (FoodTargetObject != null)
+        {
+            DistToFood = Vector3.Distance(this.transform.position, FoodTargetObject.transform.position);
+        }
+        else
+        {
+            DistToFood = -1;
+            Debug.Log("YOO NO DIST FOOD");
+        }
+
+        if (DistToFood > 0.1 && DistToFood < 4.2f &&  _curState != MyEnums.DinoState.Idle)
+        {
+            Action_EatFood();
+            Action_WaitForFood();//anim should play through then go to idle 
+            Destroy(FoodTargetObject,4f);
+        }
+    }
 
     void Start()
     {
@@ -49,12 +78,23 @@ public class DinoBrain : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+
         if (Input.GetKey(KeyCode.M)) {
-            Action_TurnToFood();
-            Action_MoveToFood();
+            if (FoodTargetObject != null)
+            {
+                Action_WaitForFood();
+                HereIsSomeAmAM(FoodTargetObject);
+                Action_TurnToFood();
+                Action_MoveToFood();
+            }
+            else {
+                Debug.Log("no food found");
+            }
+       
         }
 
         if (!_moveToFoodUpdate) return;
+        CheckDistIfPossible();
         transform.Translate(new Vector3(FoodDirection,0,0) * speed);
 
     }
